424199bc1a07d056e2c61abfadf7453ca8c89f91
Barry <baryflawless@yahoo.com>
2022-02-11 11:38:18 +0530

    moved the image tag inside div

--- src/App.tsx
+++ src/App.tsx
@@ -149,12 +149,12 @@ function App() {
   }
 
   return (
+    <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
     <div className="flex flex-wrap justify-center">
       <div className="w-6/12 sm:w-4/12 px-4">
         <img src="https://www.thisisbarry.com/wp-content/uploads/SiteFiles/this-is-barry-logo-colour.png" alt="..." className="shadow rounded max-w-full h-auto align-middle border-none" />
       </div>
     </div>
-    <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div className="flex w-80 mx-auto items-center mb-8 mt-12">
         <h1 className="text-xl ml-2.5 grow font-bold dark:text-white">{GAME_TITLE}</h1>
         <SunIcon
